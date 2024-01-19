d23ffe0df2fb5dea48c3c3e3553713bd6cd5807f
Israel Jv <hectorjv97@gmail.com>
2024-01-19 02:00:13 -0600

    Se implementa el códdigo para los botones anterior, obtener pokemon y siguiente

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+const API_URL = 'https://pokeapi.co/api/v2/';
 
-/* 
-    CONSTRUI ESTA CARD PARA VER COMO PODRÍA QUEDAR
+// Quita las cards de consultas previas.
+const reinciarCards = () => {
+    const contenedorCards = document.getElementById('row-id');
+    while (contenedorCards.firstChild) 
+        contenedorCards.firstChild.remove();
+    
+}
 
-<div class="row gy-3 my-3">
-                <div class="col-sm-6 col-md-4 col-lg-3">
-                    <div class="card" >
-                        <img src="./perro-javascript.png" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 id="name-pokemon" class="card-title text-center">Nombre de pokemon</h5>
-                          <h6 id="id-pokemon" class="card-subtitle mb-2 text-body-secondary text-center">Id pokemon</h6>
-                          <p id="weight-pokemon" class="card-text text-center">Peso</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-*/
 
+// Crea una card con los datos proporcionados
 const cardPokemon = ({urlImg, altImg, namePokemon, idPokemon, weightPokemon}) => {
     // Creación de elementos
-    const row = document.createElement('div');
     const col = document.createElement('div');
     const card = document.createElement('div');
     const img = document.createElement('img');
@@ -29,7 +21,6 @@ const cardPokemon = ({urlImg, altImg, namePokemon, idPokemon, weightPokemon}) =>
     const text = document.createElement('p');
 
     // Clases
-    row.className = 'row gy-3 my-3';
     col.className = 'col-sm-6 col-md-4 col-lg-3';
     card.className = 'card';
     img.className = 'card-img-top';
@@ -57,31 +48,40 @@ const cardPokemon = ({urlImg, altImg, namePokemon, idPokemon, weightPokemon}) =>
     card.appendChild(img);
     card.appendChild(cardBody);
     col.appendChild(card);
-    row.appendChild(col);
 
     // Se añade al documento
-    document.getElementById('pokemon-container').appendChild(row);
+    document.getElementById('row-id').appendChild(col);
 }
 
-document.getElementById('btn-show-pokemons').addEventListener('click', async () => {
-    const pokemons = await fetchPokemon(API_URL);
-    const arrayUrlsPokemonsPromise = pokemons.results.map(pokemon => fetchPokemon(pokemon.url));
-    const arrayUrlsPokemons = await Promise.all(arrayUrlsPokemonsPromise);
+// Obtiene todos los datos de los pokemons
+const getDataAPI = async ( complementURL = '' ) => {
+    try {
+        // Se manda a llamar los datos de la API
+        const pokemons = await fetchPokemon(`${API_URL}${complementURL}`);
+        // Se obtiene un arreglo de promesas para cada URL de Pokémon
+        const arrayPokemonsPromise = pokemons.results.map(pokemon => fetchPokemon(pokemon.url));
+        // Se espera a que todas las promesas se resuelvan
+        const arrayPokemons = await Promise.all(arrayPokemonsPromise);
+        // Devuelve el arreglo de Pokémon
+        return arrayPokemons;
+    } catch (error) {
+        // Manejo de errores
+        console.error("Error al obtener datos de la API:", error);
+    }
+}
 
-    const dataPokemons = arrayUrlsPokemons.map(infoPokemon => {
-        return infoPokemon = {
-            namePokemon: infoPokemon.name,
-            idPokemon: infoPokemon.id,
-            weightPokemon: infoPokemon.weight,
-            urlImg: infoPokemon.sprites.back_default,
-            altImg: infoPokemon.name + " image"
-        }
-    });
-    
-    dataPokemons.forEach(pokemon => cardPokemon(pokemon));
-    saveDataLocalStorage(dataPokemons);
-})
+// Filtra la información que vamos a estar utilizando
+const filterInfoPokemon = ( infoPokemon ) => {
+    return  {
+        namePokemon: infoPokemon.name,
+        idPokemon: infoPokemon.id,
+        weightPokemon: infoPokemon.weight,
+        urlImg: infoPokemon.sprites.back_default,
+        altImg: infoPokemon.name + " image"
+    }
+}
 
+// Consulta a la API
 const fetchPokemon = async ( rute ) => {
     try {
         const data = await fetch(rute);
@@ -92,20 +92,67 @@ const fetchPokemon = async ( rute ) => {
     }
 }
 
-const saveDataLocalStorage = ( data ) => {
-    console.log(data); // MENSAJE EN CONSOLA CON DATOS DEL ARREGLO.
-    console.log(JSON.stringify(data)); // MENSAJE EN CONSOLA CON ARREGLO VACÍO.
-    localStorage.setItem("list-pokemons", JSON.stringify(data));
+// Botón que muestra todos los pokemons
+document.getElementById('btn-show-pokemons').addEventListener('click', async () => {
+    reinciarCards()
+    const arrayPokemons = await getDataAPI('pokemon');
+    // Se construye un array con los datos que necesitamos
+    const dataPokemons = arrayPokemons.map(infoPokemon => {
+        return filterInfoPokemon(infoPokemon);
+    });
+    // Se crean las cards en el front
+    dataPokemons.forEach(pokemon => cardPokemon(pokemon));
+})
+
+// Botón que obtiene un solo pokemon
+document.getElementById('get-btn').addEventListener('click', async () =>  {
+    reinciarCards()
+    const text = document.getElementById('poke-name').value.toLowerCase();
+    const pokemonFull = await fetchPokemon(`${API_URL}pokemon/${text}`);
+    const pokemon = filterInfoPokemon(pokemonFull);
+    cardPokemon(pokemon);
+    saveIdLocalStorage(pokemon.idPokemon);
+})
+
+// Botón que obtiene el pokemon previo
+document.getElementById('previous-btn').addEventListener('click', async () => {
+    const idPokemon = parseInt(localStorage.getItem("id-pokemon"));
+    getPokemon(idPokemon - 1);
+})
+
+// Botón que obtiene el pokemon siguiente
+document.getElementById('next-btn').addEventListener('click', async () => {
+    const idPokemon = parseInt(localStorage.getItem("id-pokemon"));
+    getPokemon(idPokemon + 1);
+})
+
+const getPokemon = async ( idPokemon) => {
+    try {
+        reinciarCards();
+        const pokemonFull = await fetchPokemon(`${API_URL}pokemon/${idPokemon}`);
+        const pokemon = filterInfoPokemon(pokemonFull);
+        cardPokemon(pokemon);
+        saveIdLocalStorage(idPokemon);
+    } catch (error) {
+        console.log("No se encontró ningún pokemon.");
+    }
 }
 
-const getDataLocalStorage = () => {
-    const dataPokemons = JSON.parse(localStorage.getItem("list-pokemons"));
-    console.log(dataPokemons, dataPokemons.length);
-    if (dataPokemons){
-        dataPokemons.forEach(pokemon => cardPokemon(pokemon));
+// Guarda el id del pokemon en el localStorage
+const saveIdLocalStorage = ( idPokemon ) => {
+    localStorage.setItem("id-pokemon", idPokemon);
+}
+
+// Obtiene el id guardado en el localStore y busca el pokemon.
+const getDataLocalStorage = async () => {
+    const idPokemon = localStorage.getItem("id-pokemon");
+    if (idPokemon){
+        getPokemon(idPokemon);
     }else {
         console.log("No hay datos en el local storage");
     }
 }
 
-getDataLocalStorage()
+
+
+getDataLocalStorage();
